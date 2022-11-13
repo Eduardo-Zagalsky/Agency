e209961a3edc02318fa0306141b647626a95a54e
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-11-13 13:31:01 -0600

    added to the form, fields and validators

--- forms.py
+++ forms.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField, TextAreaField
+from wtforms import StringField, IntegerField, BooleanField, TextAreaField, SelectField
+from wtforms.validators import URL, InputRequired, Optional, NumberRange
 
 
 class AddPetForm(FlaskForm):
-    name = StringField("Pet Name")
-    species = StringField("Species")
-    photo_url = StringField("Photo")
-    age = IntegerField("Age")
-    notes = TextAreaField("Notes")
+    name = StringField("Pet Name", validators=[InputRequired()])
+    species = SelectField("Species", validators=[InputRequired()], choices=[
+                          ('dog', 'Dog'), ('cat', 'Cat'), ('porcupine', 'Porcupine')])
+    photo_url = StringField("Photo", validators=[URL(
+        require_tld=False, message="Must be an IMG Address"), Optional()])
+    age = IntegerField("Age", validators=[
+                       Optional(), NumberRange(min=0, max=30)])
+    notes = TextAreaField("Notes", validators=[Optional()])
     available = BooleanField("Available")
