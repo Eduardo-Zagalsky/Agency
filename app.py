985e16ec8e2f2b157338e99b59f9c132d9e6fd5b
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-11-13 15:17:28 -0600

    added the pet to the database

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///agency'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "secret"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
@@ -23,13 +23,23 @@ def homepage():
 @app.route("/add", methods=["GET", "POST"])
 def pet_form():
     form = AddPetForm()
-    if form.validate_on_submit:
+    if form.validate_on_submit():
         name = form.name.data
         species = form.species.data
         photo_url = form.photo_url.data
         age = form.age.data
         notes = form.notes.data
         available = form.available.data
+        new_pet = Pet(name=name, species=species, photo_url=photo_url,
+                      age=age, notes=notes, available=available)
+        db.session.add(new_pet)
+        db.session.commit()
         return redirect("/")
     else:
-        return render_template("pet-form.html")
+        return render_template("pet-form.html", form=form)
+
+
+@app.route("/<int:pet_id>")
+def detail_page(pet_id):
+    pet = Pet.query.get_or_404(pet_id)
+    return render_template("details.html", pet=pet)
